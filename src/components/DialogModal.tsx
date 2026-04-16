757ae60f24254d0658b5e418efc8d74be38a1c76
LuisFerCedillo <luis.cedillo@uabc.edu.mx>
2026-04-15 21:53:37 -0700

    Proyecto citas

---
+++ src/components/DialogModal.tsx
@@ -0,0 +1,77 @@
+import { type MouseEvent, useEffect, useRef } from "react";
+
+const isClickInsideRectangle = (e: MouseEvent, element: HTMLElement) => {
+  const r = element.getBoundingClientRect();
+
+  return (
+    e.clientX > r.left &&
+    e.clientX < r.right &&
+    e.clientY > r.top &&
+    e.clientY < r.bottom
+  );
+};
+
+type Props = {
+  title: string;
+  isOpened: boolean;
+  onProceed: () => void;
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+const DialogModal = ({
+  title,
+  isOpened,
+  onProceed,
+  onClose,
+  children,
+}: Props) => {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isOpened) {
+      ref.current?.showModal();
+      document.body.classList.add("modal-open"); // prevent bg scroll
+    } else {
+      ref.current?.close();
+      document.body.classList.remove("modal-open");
+    }
+  }, [isOpened]);
+
+  const proceedAndClose = () => {
+    onProceed();
+    onClose();
+  };
+
+  return (
+    <dialog
+      ref={ref}
+      onCancel={onClose}
+      onClick={(e:any) =>
+        ref.current && !isClickInsideRectangle(e, ref.current) && onClose()
+      }
+      className="w-[400px] rounded-lg border border-gray-400 p-6 backdrop:bg-black/30"
+    >
+      <h3 className="text-xl font-bold mb-4">{title}</h3>
+
+      <div className="mb-6">{children}</div>
+
+      <div className="flex gap-5">
+        <button 
+          onClick={proceedAndClose}
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
+        >
+          Proceed
+        </button>
+        <button 
+          onClick={onClose}
+          className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition-colors"
+        >
+          Close
+        </button>
+      </div>
+    </dialog>
+  );
+};
+
+export default DialogModal;
---
+++ src/components/Paciente.tsx
@@ -0,0 +1,66 @@
+import type { Patient } from "../types"
+import PacienteDetalle from "./PacienteDetalle"
+import { usePacienteStore } from '../store/store'
+import DialogModal from "./DialogModal"
+import { useState } from "react"
+
+
+type PacienteProps = {
+    paciente: Patient
+}
+
+const Paciente = ({ paciente }: PacienteProps) => {
+    const [isOpened, setIsOpened] = useState(false);
+
+  const onProceed = () => {
+    handleClickEliminar();
+  };
+  
+    const eliminarPaciente = usePacienteStore((state) => state.eliminarPaciente)
+    const establecerPacienteActivo = usePacienteStore((state) => state.establecerPacienteActivo)
+    
+
+    const handleClickEliminar = () => {
+        eliminarPaciente(paciente.id)
+    }
+
+    const handleClickEditar = () => {
+        establecerPacienteActivo(paciente)
+    }
+
+    return (
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+            <PacienteDetalle label="ID" data={paciente.id} />
+            <PacienteDetalle label="Nombre" data={paciente.name} />
+            <PacienteDetalle label="Propietario" data={paciente.caretaker} />
+            <PacienteDetalle label="Email" data={paciente.email} />
+            <PacienteDetalle label="Fecha Alta" data={paciente.date || ''} />
+            <PacienteDetalle label="Síntomas" data={paciente.symptoms} />
+
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={handleClickEditar}
+                >Editar</button>
+
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => setIsOpened(true)}
+                >Eliminar</button>
+
+                <DialogModal
+                    title="Dialog modal example"
+                    isOpened={isOpened}
+                    onProceed={onProceed}
+                    onClose={() => setIsOpened(false)}
+                >
+                    <p>To close: click Close, press Escape, or click outside.</p>
+                </DialogModal>
+            </div>
+        </div>
+    )
+}
+
+export default Paciente
